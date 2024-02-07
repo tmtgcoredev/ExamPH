54995236e31e1f42dbcefffbf06e43729256d869
James Macklean Lusuegro <jamesmacklean27@gmail.com>
2024-02-07 19:40:56 +0800

    ,

--- examPH/settings.py
+++ examPH/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     # os.path.join(BASE_DIR, 'static'),
-    '/usr/local/lib/python3.11/dist-packages/django/contrib/admin/static')
+    '/usr/local/lib/python3.11/dist-packages/django/contrib/admin/static',)
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
